49c9e3ddf36ed69aaa8902a051afd1ca276fd0e0
Rastko Jokic <rale01jokic@gmail.com>
2023-03-31 16:10:51 +0200

    Dodavanje pusaka na ekran i pracenje kursora

--- projekat-main/main.py
+++ projekat-main/main.py
@@ -20,8 +20,33 @@ level = 1
 for i in range(1, 4):
     bgs.append(pygame.image.load(f'projekat-main/assets/bgs/{i}.png'))
     banners.append(pygame.image.load(f'projekat-main/assets/banners/{i}.png'))
-    guns.append(pygame.image.load(f'projekat-main/assets/guns/{i}.png'))
+    guns.append(pygame.transform.scale(pygame.image.load(f'projekat-main/assets/guns/{i}.png'), (100, 100)))
 
+#metoda za pistolj i rotaciju njegovu
+def draw_gun():
+    mouse_pos = pygame.mouse.get_pos()
+    gun_point = (WIDTH / 2, HEIGHT - 200)
+    #boja kursora
+    lasers = ['red', 'green', 'orange']
+    clicks = pygame.mouse.get_pressed()
+    if mouse_pos[0] != gun_point[0]:
+        slope = (mouse_pos[1] - gun_point[1]) / (mouse_pos[0] - gun_point[0])
+    else:
+        slope = -100000
+    angle = math.atan(slope)
+    rotation = math.degrees(angle)
+    if mouse_pos[0] < WIDTH / 2:
+        gun = pygame.transform.flip(guns[level - 1], True, False)
+        if mouse_pos[1] < 600:
+            screen.blit(pygame.transform.rotate(gun, 90 - rotation), (WIDTH / 2 - 90, HEIGHT - 250))
+            if clicks[0]:
+                pygame.draw.circle(screen, lasers[level - 1], mouse_pos, 5)
+    else:
+        gun = guns[level - 1]
+        if mouse_pos[1] < 600:
+            screen.blit(pygame.transform.rotate(gun, 270 - rotation), (WIDTH / 2 - 30, HEIGHT - 250))
+            if clicks[0]:
+                pygame.draw.circle(screen, lasers[level - 1], mouse_pos, 5)
 
 
 
@@ -35,6 +60,10 @@ while run:
     screen.fill('black')
     screen.blit(bgs[level-1], (0,0))
     screen.blit(banners[level-1], (0,HEIGHT - 200))
+
+
+    if level > 0 :
+        draw_gun()
 
     #ako smo kliknuli X i zatvorili prozor prekida se igrica
     for event in pygame.event.get():

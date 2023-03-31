230f8a0262c9d005834186b5b81cd7736e6cbaf6
Rastko Jokic <rale01jokic@gmail.com>
2023-03-31 15:19:48 +0200

    Priprema gotova

--- projekat-main/main.py
+++ projekat-main/main.py
@@ -1,2 +1,16 @@
+#paketi koje cemo koristiti
 import pygame
 import math
+
+#bazicna podesavanja ekrana, fps-a, tajmera i nizova objekata u nasoj igri
+pygame.init()
+fps = 60
+timer = pygame.time.Clock()
+font = pygame.font.Font('assets/font/myFont.ttf', 32)
+WIDTH = 900
+HEIGHT = 800
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
+bgs = []
+banners = []
+guns = []
+level = 0
